4976fe1725683093a72ce3b0cd417adaf534e5d6
cjuliek <cao.juliek@gmail.com>
2022-11-18 15:48:15 -0800

    commit changes

--- server.js
+++ server.js
@@ -1,6 +1,25 @@
 const express = require("express");
-const PORT = 8000;
+const bodyParser = require('body-parser');
 const app = express();
+const PORT = 8000;
+const path = require('path');
+app.use(bodyParser.json());
+
+
+
+
+// -- view engine configuration -- // 
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
+
+
+
+
+
+
+
+// -- Live Server -- // 
 app.listen(PORT, async () => {
   console.log(`server up on port ${PORT}`);
 });
