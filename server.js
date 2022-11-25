814821f703a38ed728bbd39481d9d8450f37f378
cjuliek <cao.juliek@gmail.com>
2022-11-25 15:11:06 -0800

    task list input section and buttons

--- server.js
+++ server.js
@@ -5,9 +5,6 @@ const PORT = 8000;
 const path = require('path');
 app.use(bodyParser.json());
 
-
-
-
 // -- view engine configuration -- // 
 app.get('/', (req, res) => {
   res.send('Hello World')
